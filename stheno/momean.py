b5ac663ae95f6934a33ee552804c65f418397ea6
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-13 23:04:04 +0100

    Add MultiOutputMean

---
+++ stheno/momean.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import logging
+
+from lab import B
+from plum import Dispatcher, Self, Referentiable, type_parameter
+
+from .cache import cache, Cache
+from .graph import At
+from .mean import Mean
+
+__all__ = []
+
+log = logging.getLogger(__name__)
+
+
+class MultiOutputMean(Mean, Referentiable):
+    """A generic multi-output mean.
+
+    Args:
+        \*ps (instance of :class:`.graph.GP`): Processes that make up the
+            multi-valued process.
+    """
+    dispatch = Dispatcher(in_class=Self)
+
+    def __init__(self, *ps):
+        self.means = ps[0].graph.means
+        self.ps = ps
+
+    @dispatch(B.Numeric, Cache)
+    @cache
+    def __call__(self, x, B):
+        return B.concat([self.means[p](x, B) for p in self.ps], axis=0)
+
+    @dispatch(At, Cache)
+    @cache
+    def __call__(self, x, B):
+        return self.means[type_parameter(x)](x.get(), B)
+
+    @dispatch({tuple, list}, Cache)
+    @cache
+    def __call__(self, x, B):
+        return B.concat([self(xi, B) for xi in x], axis=0)
+
+    def __str__(self):
+        ks = [str(self.means[p]) for p in self.ps]
+        return 'MultiOutputMean({})'.format(', '.join(ks))
---
+++ tests/test_momean.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+from stheno.graph import Graph, GP, At
+from stheno.kernel import EQ
+from stheno.momean import MultiOutputMean
+# noinspection PyUnresolvedReferences
+from . import eq, neq, lt, le, ge, gt, raises, call, ok, eprint
+
+
+def test_momean():
+    m = Graph()
+    p1 = GP(1 * EQ(), lambda x: 2 * x, graph=m)
+    p2 = GP(2 * EQ().stretch(2), 1, graph=m)
+
+    mom = MultiOutputMean(p1, p2)
+    ms = m.means
+
+    x = np.linspace(0, 1, 10)
+
+    yield eq, str(mom), 'MultiOutputMean(<lambda>, 1)'
+
+    yield assert_allclose, mom(x), \
+          np.concatenate([ms[p1](x), ms[p2](x)], axis=0)
+    yield assert_allclose, mom(At(p1)(x)), ms[p1](x)
+    yield assert_allclose, mom(At(p2)(x)), ms[p2](x)
+    yield assert_allclose, mom((At(p2)(x), At(p1)(x))), \
+          np.concatenate([ms[p2](x), ms[p1](x)], axis=0)
