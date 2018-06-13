25f900a6706a5bfe1cb668b3167295964bdd33cc
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-13 16:28:29 +0100

    Add approximate integration example

---
+++ readme_example5.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from stheno import GP, EQ, Delta, model
+
+# Define points to predict at.
+x = np.linspace(0, 10, 200)
+x_obs = np.linspace(0, 10, 10)
+
+# Construct the model.
+f = 0.7 * GP(EQ()).stretch(1.5)
+e = 0.2 * GP(Delta())
+
+# Construct derivatives via finite differences.
+df = f.diff_approx(1)
+ddf = f.diff_approx(2)
+dddf = f.diff_approx(3) + e
+
+# Fix the integration constants.
+model.condition(f @ 0, 1)
+model.condition(df @ 0, 0)
+model.condition(ddf @ 0, -1)
+
+# Sample observations.
+y_obs = np.sin(x_obs) + 0.2 * np.random.randn(*x_obs.shape)
+
+# Condition on the observations to make predictions.
+model.condition(dddf @ x_obs, y_obs)
+
+# And make predictions.
+pred_iiif = f.predict(x)
+pred_iif = df.predict(x)
+pred_if = ddf.predict(x)
+pred_f = dddf.predict(x)
+
+
+# Plot result.
+def plot_prediction(x, f, pred, x_obs=None, y_obs=None):
+    plt.plot(x.squeeze(), f.squeeze(), label='True', c='tab:blue')
+    if x_obs is not None:
+        plt.scatter(x_obs.squeeze(), y_obs.squeeze(),
+                    label='Observations', c='tab:red')
+    mean, lower, upper = pred
+    plt.plot(x.squeeze(), mean, label='Prediction', c='tab:green')
+    plt.plot(x.squeeze(), lower, ls='--', c='tab:green')
+    plt.plot(x.squeeze(), upper, ls='--', c='tab:green')
+    plt.legend()
+
+
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 2, 1)
+plt.title('Function')
+plot_prediction(x, np.sin(x), pred_f, x_obs=x_obs, y_obs=y_obs)
+plt.legend()
+
+plt.subplot(2, 2, 2)
+plt.title('Integral of Function')
+plot_prediction(x, -np.cos(x), pred_if)
+plt.legend()
+
+plt.subplot(2, 2, 3)
+plt.title('Second Integral of Function')
+plot_prediction(x, -np.sin(x), pred_iif)
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.title('Third Integral of Function')
+plot_prediction(x, np.cos(x), pred_iiif)
+plt.legend()
+
+plt.show()
