f02251053de99ee481ac6dda9e313e66b153363d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-17 16:33:58 +0100

    Spelling

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import find_packages, setup
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+with open('LICENCE') as f:
+    licence = f.read()
 
 setup(
     name='stheno',
@@ -18,6 +18,6 @@ setup(
     author='Wessel Bruinsma',
     author_email='wessel.p.bruinsma@gmail.com',
     url='https://github.com/wesselb/stheno',
-    license=license,
+    license=licence,
     packages=find_packages(exclude=('tests', 'docs'))
 )
