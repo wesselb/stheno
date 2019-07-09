8e0017233db5e782ff5053cde0395b1630cff645
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-07-09 22:43:59 +0100

    Delete stheno.tf

--- stheno/tf.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import  # pragma: no cover
-
-# noinspection PyUnresolvedReferences
-import lab.tensorflow
-
-# noinspection PyUnresolvedReferences
-from . import *  # pragma: no cover
