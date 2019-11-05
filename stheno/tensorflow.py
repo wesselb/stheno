2c068e47bb999f0af23f73502ef88156c0ef2704
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-11-05 13:48:03 +0000

    Intercept __mul__ and __add__ for TF Variable

--- stheno/tensorflow.py
+++ stheno/tensorflow.py
@@ -7,7 +7,7 @@ import logging
 # noinspection PyUnresolvedReferences
 import lab.tensorflow
 from plum import Dispatcher
-from tensorflow import Tensor
+from tensorflow import Tensor, Variable
 
 # noinspection PyUnresolvedReferences
 from . import *  # pragma: no cover
@@ -20,7 +20,10 @@ _dispatch = Dispatcher()
 
 # Save original methods.
 __mul__tensor = Tensor.__mul__
+__mul__variable = Variable.__mul__
+
 __add__tensor = Tensor.__add__
+__add__variable = Variable.__add__
 
 
 @_dispatch(Tensor, object)
@@ -28,7 +31,13 @@ def __mul__(self, other):
     return __mul__tensor(self, other)
 
 
-@_dispatch(Tensor, {Element, Random})
+@_dispatch(Variable, object)
+def __mul__(self, other):
+    return __add__variable(self, other)
+
+
+@_dispatch.multi((Tensor, {Element, Random}),
+                 (Variable, {Element, Random}))
 def __mul__(self, other):
     return other.__rmul__(self)
 
@@ -38,11 +47,20 @@ def __add__(self, other):
     return __add__tensor(self, other)
 
 
-@_dispatch(Tensor, {Element, Random})
+@_dispatch(Variable, object)
+def __add__(self, other):
+    return __add__variable(self, other)
+
+
+@_dispatch.multi((Tensor, {Element, Random}),
+                 (Variable, {Element, Random}))
 def __add__(self, other):
     return other.__radd__(self)
 
 
 # Assign dispatchable methods.
 Tensor.__mul__ = __mul__
+Variable.__mul__ = __mul__
+
 Tensor.__add__ = __add__
+Variable.__add__ = __add__
--- tests/test_boxing.py
+++ tests/test_boxing.py
@@ -32,13 +32,16 @@ def test_boxing_autograd():
         assert isinstance(obj, Element)
 
 
-@pytest.mark.parametrize('dtype, Numeric',
-                         [(torch.float64, B.TorchNumeric),
-                          (tf.float64, B.TFNumeric)])
-def test_boxing(dtype, Numeric):
+@pytest.mark.parametrize(
+    'generate, Numeric',
+    [(lambda *shape: B.randn(torch.float64, *shape), B.TorchNumeric),
+     (lambda *shape: B.randn(tf.float64, *shape), B.TFNumeric),
+     (lambda *shape: tf.Variable(B.randn(tf.float64, *shape)), B.TFNumeric)]
+)
+def test_boxing(generate, Numeric):
     k = EQ()
-    num = B.randn(dtype)
-    x = B.randn(dtype, 10)
+    num = generate()
+    x = generate(10)
 
     # Test addition and multiplication.
     assert isinstance(num + k, Element)
@@ -48,26 +51,36 @@ def test_boxing(dtype, Numeric):
     assert isinstance(num * k(x), Element)
     assert isinstance(num * num, Numeric)
 
+
+@pytest.mark.parametrize(
+    'generate, Numeric',
+    [(lambda *shape: B.randn(torch.float64, *shape), B.TorchNumeric),
+     (lambda *shape: B.randn(tf.float64, *shape), B.TFNumeric)]
+)
+def test_boxing_inplace(generate, Numeric):
+    k = EQ()
+    x = generate(10)
+
     # Test in-place addition.
-    num = B.randn(dtype)
-    num += B.randn(dtype)
+    num = generate()
+    num += generate()
     assert isinstance(num, Numeric)
 
     num += k
     assert isinstance(num, Element)
 
-    num = B.randn(dtype)
+    num = generate()
     num += k(x)
     assert isinstance(num, Element)
 
     # Test in-place multiplication.
-    num = B.randn(dtype)
-    num *= B.randn(dtype)
+    num = generate()
+    num *= generate()
     assert isinstance(num, Numeric)
 
     num *= k
     assert isinstance(num, Element)
 
-    num = B.randn(dtype)
+    num = generate()
     num *= k(x)
     assert isinstance(num, Element)
