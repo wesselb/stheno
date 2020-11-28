0f1af316496557e344824767b687c693dcc1df8c
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-11-28 18:54:31 +0000

    Rework util

--- stheno/util.py
+++ stheno/util.py
@@ -4,9 +4,27 @@ from types import FunctionType
 from lab import B
 from plum import Dispatcher
 
-__all__ = []
+__all__ = ["num_elements", "uprank"]
 
 _dispatch = Dispatcher()
+
+
+def num_elements(x):
+    """Determine the number of elements in an input.
+
+    Deals with scalars, vectors, and matrices.
+
+    Args:
+        x (tensor): Input.
+
+    Returns:
+        int: Number of elements.
+    """
+    shape = B.shape(x)
+    if shape == ():
+        return 1
+    else:
+        return shape[0]
 
 
 @_dispatch(object)
@@ -22,18 +40,8 @@ def uprank(x):
     # Simply return non-numerical inputs.
     if not isinstance(x, B.Numeric):
         return x
-
-    # Now check the rank of `x` and act accordingly.
-    rank = B.rank(x)
-    if rank > 2:
-        raise ValueError('Input must be at most rank 2.')
-    elif rank == 2:
-        return x
-    elif rank == 1:
-        return B.expand_dims(x, axis=1)
     else:
-        # Rank must be 0.
-        return B.expand_dims(B.expand_dims(x, axis=0), axis=1)
+        return B.uprank(x)
 
 
 @_dispatch(FunctionType)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,20 +1,29 @@
 import numpy as np
 import pytest
 from lab import B
+
 from stheno.graph import GP, Graph
 from stheno.input import Component
 from stheno.kernel import OneKernel, EQ
 from stheno.mean import OneMean
-from stheno.util import uprank
+from stheno.util import uprank, num_elements
+from .util import approx
 
-from .util import allclose, approx
+
+def test_num_elements():
+    assert num_elements(B.randn()) == 1
+    assert num_elements(B.randn(2)) == 2
+    assert num_elements(B.randn(2, 2)) == 2
 
 
 def test_uprank():
-    allclose(uprank(0), [[0]])
-    allclose(uprank(np.array([0])), [[0]])
-    allclose(uprank(np.array([[0]])), [[0]])
-    assert type(uprank(Component('test')(0))) == Component('test')
+    # Check upranking behaviour.
+    approx(uprank(0), [[0]])
+    approx(uprank(np.array([0])), [[0]])
+    approx(uprank(np.array([[0]])), [[0]])
+
+    # Check that it leaves objects alone.
+    assert type(uprank(Component("test")(0))) == Component("test")
 
     k = OneKernel()
 
@@ -30,11 +39,6 @@ def test_uprank():
     assert B.shape(k(np.ones((5, 2)), np.ones(5))) == (5, 5)
     assert B.shape(k(np.ones((5, 2)), np.ones((5, 2)))) == (5, 5)
 
-    with pytest.raises(ValueError):
-        k(0, np.ones((5, 2, 1)))
-    with pytest.raises(ValueError):
-        k(np.ones((5, 2, 1)))
-
     m = OneMean()
 
     assert B.shape(m(0)) == (1, 1)
@@ -42,7 +46,8 @@ def test_uprank():
     assert B.shape(m(np.ones((5, 2)))) == (5, 1)
 
     p = GP(EQ(), graph=Graph())
-    x = np.linspace(0, 10, 10)
+
+    x = B.linspace(0, 10, 10)
 
     approx(p.condition(1, 1)(1).mean, np.array([[1]]))
     approx(p.condition(x, x)(x).mean, x[:, None])
