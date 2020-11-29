c14a734ecf7a678c302b9ec319d17546a1d11420
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-11-29 16:27:33 +0000

    Add promised FDD and GP types to prevent circular imports

--- stheno/__init__.py
+++ stheno/__init__.py
@@ -1,13 +1,20 @@
-from lab import B
 # noinspection PyUnresolvedReferences
 import matrix
+# noinspection PyUnresolvedReferences
+from lab import B
+
+from plum import PromisedType
+
+PromisedFDD = PromisedType()
+PromisedGP = PromisedType()
 
 from .input import *
-from .util import *
-from .mean import *
-from .momean import *
 from .kernel import *
-from .mokernel import *
-from .random import *
 from .lazy import *
-from .graph import *
+from .mean import *
+from .measure import *
+from .mokernel import *
+from .momean import *
+from .random import *
+from .util import *
+
