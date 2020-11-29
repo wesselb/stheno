326a1147ebfb485ba35a7e284916adff6695348f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-11-29 16:27:12 +0000

    Move position of test

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -451,6 +451,22 @@ def test_sum():
     standard_kernel_tests(k)
 
 
+def test_product():
+    k = (2 * EQ().stretch(10)) * (3 * RQ(1e-2).stretch(20))
+
+    # Verify that the kernel has the right properties.
+    assert k.stationary
+
+    # Test equality.
+    assert EQ() * Linear() == EQ() * Linear()
+    assert EQ() * Linear() == Linear() * EQ()
+    assert EQ() * Linear() != EQ() * RQ(1e-1)
+    assert EQ() * Linear() != RQ(1e-1) * Linear()
+
+    # Standard tests:
+    standard_kernel_tests(k)
+
+
 def test_stretched():
     k = EQ().stretch(2)
 
@@ -547,22 +563,6 @@ def test_shifted():
     # Check passing in a list.
     k = Linear().shift(np.array([1, 2]))
     k(np.random.randn(10, 2))
-
-
-def test_product():
-    k = (2 * EQ().stretch(10)) * (3 * RQ(1e-2).stretch(20))
-
-    # Verify that the kernel has the right properties.
-    assert k.stationary
-
-    # Test equality.
-    assert EQ() * Linear() == EQ() * Linear()
-    assert EQ() * Linear() == Linear() * EQ()
-    assert EQ() * Linear() != EQ() * RQ(1e-1)
-    assert EQ() * Linear() != RQ(1e-1) * Linear()
-
-    # Standard tests:
-    standard_kernel_tests(k)
 
 
 def test_selection():
