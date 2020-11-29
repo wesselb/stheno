d28fc4d8e337f1c87391450b6468198201ac0f9e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-11-29 16:21:00 +0000

    Rework multi-output kernel

--- stheno/mokernel.py
+++ stheno/mokernel.py
@@ -1,12 +1,13 @@
 import logging
 
 from lab import B
-from plum import Dispatcher, Self, type_parameter
+from plum import Dispatcher, Self
 
-from .input import At, MultiInput, Input
+from . import PromisedFDD as FDD
+from .input import MultiInput, Input
 from .kernel import Kernel
 
-__all__ = ['MultiOutputKernel']
+__all__ = ["MultiOutputKernel"]
 
 log = logging.getLogger(__name__)
 
@@ -15,38 +16,44 @@ class MultiOutputKernel(Kernel):
     """A generic multi-output kernel.
 
     Args:
-        *ps (instance of :class:`.graph.GP`): Processes that make up the
-            multi-valued process.
+        measure (:class:`.measure.Measure`): Measure to take the kernels from.
+        *ps (:class:`.measure.GP`): Processes that make up the multi-valued process.
+
+    Attributes:
+        measure (:class:`.measure.Measure`): Measure to take the kernels from.
+        ps (tuple[:class:`.measure.GP`]): Processes that make up the multi-valued
+            process.
     """
+
     _dispatch = Dispatcher(in_class=Self)
 
-    def __init__(self, *ps):
-        self.kernels = ps[0].graph.kernels
+    def __init__(self, measure, *ps):
+        self.measure = measure
         self.ps = ps
 
     @_dispatch({B.Numeric, Input}, {B.Numeric, Input})
     def __call__(self, x, y):
-        return self(MultiInput(*(p(x) for p in self.ps)),
-                    MultiInput(*(p(y) for p in self.ps)))
+        return self(
+            MultiInput(*(p(x) for p in self.ps)), MultiInput(*(p(y) for p in self.ps))
+        )
 
-    @_dispatch(At, {B.Numeric, Input})
+    @_dispatch(FDD, {B.Numeric, Input})
     def __call__(self, x, y):
         return self(MultiInput(x), MultiInput(*(p(y) for p in self.ps)))
 
-    @_dispatch({B.Numeric, Input}, At)
+    @_dispatch({B.Numeric, Input}, FDD)
     def __call__(self, x, y):
         return self(MultiInput(*(p(x) for p in self.ps)), MultiInput(y))
 
-    @_dispatch(At, At)
+    @_dispatch(FDD, FDD)
     def __call__(self, x, y):
-        return self.kernels[type_parameter(x),
-                            type_parameter(y)](x.get(), y.get())
+        return self.measure.kernels[x.p, y.p](x.x, y.x)
 
-    @_dispatch(MultiInput, At)
+    @_dispatch(MultiInput, FDD)
     def __call__(self, x, y):
         return self(x, MultiInput(y))
 
-    @_dispatch(At, MultiInput)
+    @_dispatch(FDD, MultiInput)
     def __call__(self, x, y):
         return self(MultiInput(x), y)
 
@@ -56,42 +63,49 @@ class MultiOutputKernel(Kernel):
 
     @_dispatch({B.Numeric, Input}, {B.Numeric, Input})
     def elwise(self, x, y):
-        return self.elwise(MultiInput(*(p(x) for p in self.ps)),
-                           MultiInput(*(p(y) for p in self.ps)))
+        return self.elwise(
+            MultiInput(*(p(x) for p in self.ps)), MultiInput(*(p(y) for p in self.ps))
+        )
 
-    @_dispatch(At, {B.Numeric, Input})
+    @_dispatch(FDD, {B.Numeric, Input})
     def elwise(self, x, y):
-        raise ValueError('Unclear combination of arguments given to '
-                         'MultiOutputKernel.elwise.')
+        raise ValueError(
+            "Unclear combination of arguments given to MultiOutputKernel.elwise."
+        )
 
-    @_dispatch({B.Numeric, Input}, At)
+    @_dispatch({B.Numeric, Input}, FDD)
     def elwise(self, x, y):
-        raise ValueError('Unclear combination of arguments given to '
-                         'MultiOutputKernel.elwise.')
+        raise ValueError(
+            "Unclear combination of arguments given to " "MultiOutputKernel.elwise."
+        )
 
-    @_dispatch(At, At)
+    @_dispatch(FDD, FDD)
     def elwise(self, x, y):
-        return self.kernels[type_parameter(x),
-                            type_parameter(y)].elwise(x.get(), y.get())
+        return self.measure.kernels[x.p, y.p].elwise(x.x, y.x)
 
-    @_dispatch(MultiInput, At)
+    @_dispatch(MultiInput, FDD)
     def elwise(self, x, y):
-        raise ValueError('Unclear combination of arguments given to '
-                         'MultiOutputKernel.elwise.')
+        raise ValueError(
+            "Unclear combination of arguments given to MultiOutputKernel.elwise."
+        )
 
-    @_dispatch(At, MultiInput)
+    @_dispatch(FDD, MultiInput)
     def elwise(self, x, y):
-        raise ValueError('Unclear combination of arguments given to '
-                         'MultiOutputKernel.elwise.')
+        raise ValueError(
+            "Unclear combination of arguments given to MultiOutputKernel.elwise."
+        )
 
     @_dispatch(MultiInput, MultiInput)
     def elwise(self, x, y):
         if len(x.get()) != len(y.get()):
-            raise ValueError('MultiOutputKernel.elwise must be called with '
-                             'similarly sized MultiInputs.')
-        return B.concat(*[self.elwise(xi, yi)
-                          for xi, yi in zip(x.get(), y.get())], axis=0)
+            raise ValueError(
+                "MultiOutputKernel.elwise must be called with similarly sized "
+                "MultiInputs."
+            )
+        return B.concat(
+            *[self.elwise(xi, yi) for xi, yi in zip(x.get(), y.get())], axis=0
+        )
 
     def __str__(self):
-        ks = [str(self.kernels[p]) for p in self.ps]
-        return 'MultiOutputKernel({})'.format(', '.join(ks))
+        ks = [str(self.measure.kernels[p]) for p in self.ps]
+        return "MultiOutputKernel({})".format(", ".join(ks))
--- tests/test_mokernel.py
+++ tests/test_mokernel.py
@@ -1,82 +1,78 @@
 import lab as B
-import numpy as np
 import pytest
-from stheno.graph import Graph, GP
+
 from stheno.input import MultiInput
 from stheno.kernel import EQ
+from stheno.measure import Measure, GP
 from stheno.mokernel import MultiOutputKernel
-
-from .util import allclose
+from .util import approx
 
 
 def test_mokernel():
-    m = Graph()
-    p1 = GP(1 * EQ(), graph=m)
-    p2 = GP(2 * EQ().stretch(2), graph=m)
+    m = Measure()
+    p1 = GP(1 * EQ(), measure=m)
+    p2 = GP(2 * EQ().stretch(2), measure=m)
 
-    mok = MultiOutputKernel(p1, p2)
+    mok = MultiOutputKernel(m, p1, p2)
     ks = m.kernels
 
-    x1 = np.linspace(0, 1, 10)
-    x2 = np.linspace(1, 2, 5)
-    x3 = np.linspace(1, 2, 10)
+    x1 = B.linspace(0, 1, 10)
+    x2 = B.linspace(1, 2, 5)
+    x3 = B.linspace(1, 2, 10)
 
-    assert str(mok) == 'MultiOutputKernel(EQ(), 2 * (EQ() > 2))'
+    # Check representation.
+    assert str(mok) == "MultiOutputKernel(EQ(), 2 * (EQ() > 2))"
 
     # `B.Numeric` versus `B.Numeric`:
-    allclose(mok(x1, x2),
-             np.concatenate([np.concatenate([B.dense(ks[p1, p1](x1, x2)),
-                                             B.dense(ks[p1, p2](x1, x2))],
-                                            axis=1),
-                             np.concatenate([B.dense(ks[p2, p1](x1, x2)),
-                                             B.dense(ks[p2, p2](x1, x2))],
-                                            axis=1)], axis=0))
-    allclose(mok.elwise(x1, x3),
-             np.concatenate([ks[p1, p1].elwise(x1, x3),
-                             ks[p2, p2].elwise(x1, x3)], axis=0))
+    approx(
+        mok(x1, x2),
+        B.concat2d(
+            [ks[p1, p1](x1, x2), ks[p1, p2](x1, x2)],
+            [ks[p2, p1](x1, x2), ks[p2, p2](x1, x2)],
+        ),
+    )
+    approx(
+        mok.elwise(x1, x3),
+        B.concat(ks[p1, p1].elwise(x1, x3), ks[p2, p2].elwise(x1, x3), axis=0),
+    )
 
-    # `B.Numeric` versus `At`:
-    allclose(mok(p1(x1), x2),
-             np.concatenate([B.dense(ks[p1, p1](x1, x2)),
-                             B.dense(ks[p1, p2](x1, x2))], axis=1))
-    allclose(mok(p2(x1), x2),
-             np.concatenate([B.dense(ks[p2, p1](x1, x2)),
-                             B.dense(ks[p2, p2](x1, x2))], axis=1))
-    allclose(mok(x1, p1(x2)),
-             np.concatenate([B.dense(ks[p1, p1](x1, x2)),
-                             B.dense(ks[p2, p1](x1, x2))], axis=0))
-    allclose(mok(x1, p2(x2)),
-             np.concatenate([B.dense(ks[p1, p2](x1, x2)),
-                             B.dense(ks[p2, p2](x1, x2))], axis=0))
+    # `B.Numeric` versus `FDD`:
+    approx(mok(p1(x1), x2), B.concat(ks[p1, p1](x1, x2), ks[p1, p2](x1, x2), axis=1))
+    approx(mok(p2(x1), x2), B.concat(ks[p2, p1](x1, x2), ks[p2, p2](x1, x2), axis=1))
+    approx(mok(x1, p1(x2)), B.concat(ks[p1, p1](x1, x2), ks[p2, p1](x1, x2), axis=0))
+    approx(mok(x1, p2(x2)), B.concat(ks[p1, p2](x1, x2), ks[p2, p2](x1, x2), axis=0))
     with pytest.raises(ValueError):
         mok.elwise(x1, p2(x3))
     with pytest.raises(ValueError):
         mok.elwise(p1(x1), x3)
 
-    # `At` versus `At`:
-    allclose(mok(p1(x1), p1(x2)), ks[p1](x1, x2))
-    allclose(mok(p1(x1), p2(x2)), ks[p1, p2](x1, x2))
-    allclose(mok.elwise(p1(x1), p1(x3)), ks[p1].elwise(x1, x3))
-    allclose(mok.elwise(p1(x1), p2(x3)), ks[p1, p2].elwise(x1, x3))
+    # `FDD` versus `FDD`:
+    approx(mok(p1(x1), p1(x2)), ks[p1](x1, x2))
+    approx(mok(p1(x1), p2(x2)), ks[p1, p2](x1, x2))
+    approx(mok.elwise(p1(x1), p1(x3)), ks[p1].elwise(x1, x3))
+    approx(mok.elwise(p1(x1), p2(x3)), ks[p1, p2].elwise(x1, x3))
 
     # `MultiInput` versus `MultiInput`:
-    allclose(mok(MultiInput(p2(x1), p1(x2)), MultiInput(p2(x1))),
-             np.concatenate([B.dense(ks[p2, p2](x1, x1)),
-                             B.dense(ks[p1, p2](x2, x1))], axis=0))
+    approx(
+        mok(MultiInput(p2(x1), p1(x2)), MultiInput(p2(x1))),
+        B.concat(ks[p2, p2](x1, x1), ks[p1, p2](x2, x1), axis=0),
+    )
     with pytest.raises(ValueError):
         mok.elwise(MultiInput(p2(x1), p1(x3)), MultiInput(p2(x1)))
-    allclose(mok.elwise(MultiInput(p2(x1), p1(x3)),
-                        MultiInput(p2(x1), p1(x3))),
-             np.concatenate([ks[p2, p2].elwise(x1, x1),
-                             ks[p1, p1].elwise(x3, x3)], axis=0))
+    approx(
+        mok.elwise(MultiInput(p2(x1), p1(x3)), MultiInput(p2(x1), p1(x3))),
+        B.concat(ks[p2, p2].elwise(x1, x1), ks[p1, p1].elwise(x3, x3), axis=0),
+    )
 
-    # `MultiInput` versus `At`:
-    allclose(mok(MultiInput(p2(x1), p1(x2)), p2(x1)),
-             np.concatenate([B.dense(ks[p2, p2](x1, x1)),
-                             B.dense(ks[p1, p2](x2, x1))], axis=0))
-    allclose(mok(p2(x1), MultiInput(p2(x1), p1(x2))),
-             np.concatenate([B.dense(ks[p2, p2](x1, x1)),
-                             B.dense(ks[p2, p1](x1, x2))], axis=1))
+    # `MultiInput` versus `FDD`:
+    approx(
+        mok(MultiInput(p2(x1), p1(x2)), p2(x1)),
+        B.concat(ks[p2, p2](x1, x1), ks[p1, p2](x2, x1), axis=0),
+    )
+    approx(
+        mok(p2(x1), MultiInput(p2(x1), p1(x2))),
+        B.concat(ks[p2, p2](x1, x1), ks[p2, p1](x1, x2), axis=1),
+    )
     with pytest.raises(ValueError):
         mok.elwise(MultiInput(p2(x1), p1(x3)), p2(x1))
     with pytest.raises(ValueError):
