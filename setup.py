6d98bab9a4303529e3dacedce898453ea5117dc8
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-11-30 19:34:31 +0000

    Format setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, division, print_function
-
 from setuptools import find_packages, setup
 
-requirements = ['numpy>=1.16',
+requirements = [
+    "numpy>=1.16",
+    "fdm",
+    "plum-dispatch",
+    "backends>=0.3",
+    "backends-matrix",
+]
 
-                'fdm',
-                'plum-dispatch',
-                'backends>=0.3',
-                'backends-matrix']
-
-setup(packages=find_packages(exclude=['docs']),
-      python_requires='>=3.6',
-      install_requires=requirements,
-      include_package_data=True)
+setup(
+    packages=find_packages(exclude=["docs"]),
+    python_requires=">=3.6",
+    install_requires=requirements,
+    include_package_data=True,
+)
